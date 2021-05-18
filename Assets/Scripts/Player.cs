9d5a403d33387540e9200c7fb2cab33ba3a5eb6f
someretical <29365738+someretical@users.noreply.github.com>
2021-05-18 22:50:20 +1000

    added player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public float speed = 5f;
+	public Rigidbody2D rigidBody;
+	private Vector2 movement;
+	void Update()
+	{
+		movement.x = Input.GetAxisRaw("Horizontal");
+		movement.y = Input.GetAxisRaw("Vertical");
+		rigidBody.MovePosition(rigidBody.position + movement * speed * Time.fixedDeltaTime);
+	}
+}
