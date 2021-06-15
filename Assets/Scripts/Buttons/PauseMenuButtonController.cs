af21a417335d75bb372926297b2bcebb6669a3bb
someretical <29365738+someretical@users.noreply.github.com>
2021-06-15 18:00:21 +1000

    changed button shading

--- Assets/Scripts/Buttons/PauseMenuButtonController.cs
+++ Assets/Scripts/Buttons/PauseMenuButtonController.cs
@@ -10,13 +10,14 @@ public class PauseMenuButtonController : MonoBehaviour, IPointerEnterHandler, IP
 	void Awake()
 	{
 		textMeshPro = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+		textMeshPro.color = new Color32(144, 144, 144, 255);
 	}
 	public void OnPointerEnter(PointerEventData pointerEventData)
 	{
-		textMeshPro.color = new Color32(200, 200, 200, 255);
+		textMeshPro.color = new Color32(255, 255, 255, 255);
 	}
 	public void OnPointerExit(PointerEventData pointerEventData)
 	{
-		textMeshPro.color = new Color32(255, 255, 255, 255);
+		textMeshPro.color = new Color32(144, 144, 144, 255);
 	}
 }
